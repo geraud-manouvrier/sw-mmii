b9fceee9775a2aad46cd110945a39e7cac663d6f
Geraud <geraud.manouvrier@qande.cl>
2025-02-26 17:53:28 -0300

    v10.1.1-COL - Manejo de caché recursos estáticos
    Correcciones:
    -
    
    Cambios menores:
    -
    
    Datos
    -
    
    Properties:
    -
    
    Otros y artefactos:
    -

--- fuentes/app/src/main/java/cl/qande/mmii/app/MvcConfig.java
+++ fuentes/app/src/main/java/cl/qande/mmii/app/MvcConfig.java
@@ -7,12 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.VersionResourceResolver;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -59,12 +62,24 @@ public class MvcConfig implements WebMvcConfigurer {
     // Configuración para el versionado de recursos estáticos
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //Rutas estarían mal definidas
         registry.addResourceHandler("/resources/**")
                 .addResourceLocations("/resources/")
                 .setCachePeriod(31556926) // 1 año en segundos
                 .resourceChain(true)
                 .addResolver(new VersionResourceResolver()
                         .addContentVersionStrategy("/**")); // Agrega un hash basado en el contenido
+
+        customRegistryCache(registry, "build");
+        customRegistryCache(registry, "dist");
+        customRegistryCache(registry, "plugins");
+        customRegistryCache(registry, "syncfusion.v26.1.35");
+    }
+
+    private void customRegistryCache(ResourceHandlerRegistry registry, String baseFolder) {
+        registry.addResourceHandler("/"+baseFolder+"/**") //cómo se exponen los recursos en la URL
+                .addResourceLocations("classpath:/static/"+baseFolder+"/")    //define la ubicación de los recursos en el sistema de archivos del proyecto
+                .setCacheControl(CacheControl.maxAge(30, TimeUnit.DAYS));
     }
 
 }
