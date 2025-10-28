bc5db85fa85710d48c2ccdfe8e592d9f827eb705
Geraud <geraud.manouvrier@qande.cl>
2025-10-28 09:37:38 -0300

    v12.3.2-COL -
    Correcciones:
    - Error Cuenta RIA informada en SFL ACCT de Proteccion (se editó también SFL, no ZIP)
    
    Cambios menores:
    -
    
    Datos
    -
    
    Properties:
    -
    
    Otros y artefactos:
    -

--- fuentes/database/Incidencias.sql
+++ fuentes/database/Incidencias.sql
@@ -1,9 +1,18 @@
 --========================================================================
 --========================================================================
 --========================================================================
--- Correcciones anualziación retornos
-select *
-from rep_inv.fn_calcula_rentabilidad('901751968', null, null, null, '20251023', '20251023')
+-- Cuenta RIA informada en SFL ACCT
+select * from stage_pershing.stage_acct_file where linea like '%T9O004725%';    -- 6 líneas, todas del 20251023
+select * from stage_pershing.stage_capt_file where linea like '%T9O004725%';    --
+select * from stage_pershing.stage_gact_file where linea like '%T9O004725%';    --
+select * from stage_pershing.stage_gcus_file where linea like '%T9O004725%';    --
+select * from stage_pershing.stage_gmon_file where linea like '%T9O004725%';    --
+select * from stage_pershing.stage_gtol_file where linea like '%T9O004725%';    --
+select * from stage_pershing.stage_isca_file where linea like '%T9O004725%';    --
+select * from stage_pershing.stage_isrc_file where linea like '%T9O004725%';    --
+select * from stage_pershing.stage_potl_file where linea like '%T9O004725%';    --
+select * from stage_pershing.stage_ptld_file where linea like '%T9O004725%';    --
+
 
 
 --========================================================================
